bc0a4527fb0fae9e25cc16ed1d8c102f8d57fe15
Ömer Gözüaçık <ogozuacik@gmail.com>
2020-07-31 00:44:56 +0300

    Add files via upload

---
+++ 5_kategori_tahmin.py
@@ -0,0 +1,30 @@
+# Ömer Gözüaçık
+
+import pickle
+import pandas as pd
+import pre_processing_tr as pr
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Sınıflandırılmak istenen metin girilir.
+text = input("Sınıflandırmak istediğiniz haber metnini giriniz. \n\n")
+
+# Metin ön işleyiciden geçirildikten sonra sonuç
+text_processed = pr.pre_process(text)
+print('\n Ön işleme sonucu metinin son hali: \n')
+print(text_processed)
+
+#Önceden eğitilmiş modellerin yüklenmesi
+filename = 'modeller/5-kategori_vocab.sav'
+loaded_vocab = pickle.load(open(filename, 'rb'))
+
+cv = CountVectorizer(vocabulary=loaded_vocab)
+text_vector = cv.fit_transform([text_processed])
+
+filename = 'modeller/5-kategori.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+class_mapping = {0: 'Diğer', 1: 'Ekonomi', 2: 'Siyaset', 3: 'Spor', 4: 'Teknoloji_Bilim'}
+
+#Tahmin aşaması
+pred = loaded_model.predict(text_vector)
+print(f"\nKategori tahmini: {class_mapping[pred[0]]}")
